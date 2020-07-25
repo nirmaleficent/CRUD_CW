0ed805e15f770c104a9f672895c3b44a62759dab
Nirmal <nirmal.m.dalal@gmail.com>
2020-07-25 11:27:32 -0600

    Initial Commit

---
+++ CRUD_CW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CRUD_CW
+//
+//  Created by Nirmal Dalal on 2020-07-25.
+//  Copyright © 2020 Nirmal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CRUD_CWTests/CRUD_CWTests.swift
@@ -0,0 +1,34 @@
+//
+//  CRUD_CWTests.swift
+//  CRUD_CWTests
+//
+//  Created by Nirmal Dalal on 2020-07-25.
+//  Copyright © 2020 Nirmal. All rights reserved.
+//
+
+import XCTest
+@testable import CRUD_CW
+
+class CRUD_CWTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
